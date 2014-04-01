08e30dfc30cdcb6ee7b4858475d6f9aebe477eb0
hiroyuki.mizukami <majestaaa@gmail.com>
2014-04-01 20:39:45 +0900

    Refer localhost for rabbitmq

--- chef-repo/site-cookbooks/sensu-server/attributes/default.rb
+++ chef-repo/site-cookbooks/sensu-server/attributes/default.rb
@@ -1,6 +1,6 @@
 default.sensu.use_ssl = false
-default["sensu"]["use_embedded_ruby"] = true 
-default["sensu"]["rabbitmq"]["host"] = "192.168.33.10"
+default["sensu"]["use_embedded_ruby"] = true
+default["sensu"]["rabbitmq"]["host"] = "localhost"
 default["sensu"]["rabbitmq"]["port"] = 5672
 default["sensu"]["rabbitmq"]["user"] = "admin"
 default["sensu"]["rabbitmq"]["password"] = "password"
@@ -14,7 +14,7 @@ default["sensu"]["dashboard"]["port"] = 8080
 default["sensu"]["dashboard"]["user"] = "admin"
 default["sensu"]["dashboard"]["password"] = "password"
 default["sensu"]["graphite"]["host"] = "localhost"
-default["sensu"]["graphite"]["port"] = 2003 
+default["sensu"]["graphite"]["port"] = 2003
 
 default["sensu-server-wrapper"]["iptables_enabled"] = false
-default["sensu-server-wrapper"]["use_apache"] = false 
+default["sensu-server-wrapper"]["use_apache"] = false
